0d423156bd417b25fb7c2732ed9c792b4d1d9d8c
smrrvv1 <samira.b@compasscollege.art>
2026-03-09 02:29:12 +0600

    add types for country

---
+++ src/types/country.ts
@@ -0,0 +1,12 @@
+export interface ICountry {
+    name: string;
+    alpha3Code: string;
+    capital?: string;
+    region?: string;
+    population?: number;
+    area?: number;
+    nativeName?: string;
+    numericCode?: string;
+    flag?: string;
+    subregion?: string;
+  }
