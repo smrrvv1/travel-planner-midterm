d8c79fd2ecd2d877e9f877b550c2e180edc6e34e
smrrvv1 <samira.b@compasscollege.art>
2026-03-09 02:59:58 +0600

    add App.tsx and styles

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.css'
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <h1>Планировщик путешествий</h1>
+    </div>
+  )
+}
+
+export default App;
